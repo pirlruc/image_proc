68a654cac0e3d3b66e7b6774fb6e0d8537536c02
pirlruc <pirlruc@gmail.com>
2021-08-20 19:58:10 +0000

    Renamed folder and added version to improc_corecv module

---
+++ modules/corecv/version/main.cpp
@@ -0,0 +1,15 @@
+#include <improc_corecv_config.h>
+
+#include <iostream>
+
+int main()
+{
+    std::cout   << "improc_corecv Version: " 
+                << IMPROC_CORECV_VERSION_MAJOR 
+                << "."
+                << IMPROC_CORECV_VERSION_MINOR
+                << "."
+                << IMPROC_CORECV_VERSION_PATCH
+                << std::endl;
+    return 0;
+}
