448e7e89fba41214833e8ea4c0d3ccf0a068a166
Nuno Monteiro <pirlruc@gmail.com>
2023-04-28 02:08:10 +0000

    Added vscode extensions and updated submodules

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension mhutchie.git-graph
+code --install-extension eamodio.gitlens
+code --install-extension gruntfuggly.todo-tree
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension github.codespaces
